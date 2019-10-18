6c3033947930688d33fa3fa73b1e074a49b92daf
Kevin Tu <tu02.kevin@gmail.com>
2019-10-18 14:39:17 -0400

    Create motortheory.js

---
+++ motortheory.js
@@ -0,0 +1,48 @@
+const int pwr = 1; //pwm
+const int motor_in_1 = 6;
+const int motor_in_2 = 7;
+const int motor_in_3 = 8;
+const int motor_in_4 = 9;
+
+void setup()
+{
+  pinMode(pwr, OUTPUT);
+  pinMode(motor_in_1 , OUTPUT);
+  pinMode(motor_in_2 , OUTPUT);
+  pinMode(motor_in_3 , OUTPUT);
+  pinMode(motor_in_4 , OUTPUT);
+}
+
+void looop()
+{
+  digitalWrite(motor_in_1,HIGH);
+  digitalWrite(motor_in_2,LOW);
+  digitalWrite(motor_in_3,HIGH);
+  digitalWrite(motor_in_4,LOW);
+  analogWrite(pwr,255) ;
+
+  delay(3000);
+
+  digitalWrite(motor_in_1,HIGH);
+  digitalWrite(motor_in_2,HIGH);
+  digitalWrite(motor_in_3,HIGH);
+  digitalWrite(motor_in_4,HIGH);
+
+  delay(1000) ;
+
+  digitalWrite(motor_in_1,LOW) ;
+  digitalWrite(motor_in_2,HIGH) ;
+  digitalWrite(motor_in_3,LOW) ;
+  digitalWrite(motor_in_4,HIGH) ;
+
+  delay(3000) ;
+
+
+  digitalWrite(motor_in_1,HIGH);
+  digitalWrite(motor_in_2,HIGH);
+  digitalWrite(motor_in_3,HIGH);
+  digitalWrite(motor_in_4,HIGH);
+
+  delay(1000) ;
+}
+
