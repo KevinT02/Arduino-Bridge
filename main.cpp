632d8e3ed0657529685005652d3b9643375b1f82
Kevin Tu <tu02.kevin@gmail.com>
2019-11-14 20:05:37 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -33,24 +33,55 @@ void setup() {
 
 void extend(int time1, int time2)
 {
-  digitalWrite(motor2for, HIGH);
-  delay(time1);
-  digitalWrite(motor2for, LOW);
+  /*Initializes the motors to begin the extending motion.
+  Begins by extending the second motor long enough to undo
+  the spun rope around the wheel from the retracting motion.
+  First phase is stopped before beginning the second. The 
+  first motor begins to extend and the second motor begins to
+  retract to unfold the bridge and then stops it after it has 
+  unfolded. */
+  
+  //begins turning second motor forward
+  digitalWrite(motor2for, HIGH);  //turns second motor forward
+  delay(time1);  //time second motors spins to complete first phase
+  digitalWrite(motor2for, LOW);  //stops the second motor 
+  
+  //begins turning first motor forward and second motor backward
   digitalWrite(motor1for, HIGH);
   digitalWrite(motor2bac, HIGH);
-  delay(time2);
+  
+  delay(time2);  //time both motors spins to complete second phase 
+  
+  //stops both motors
   digitalWrite(motor2bac, LOW);
   digitalWrite(motor1for,LOW);
 }
 
 void retract(int time1, int time2)
 {
-  digitalWrite(motor1bac,HIGH);
-  delay(time1);
-  digitalWrite(motor1bac, LOW);
+  /*Initializes the motors to begin the retracting motion.
+  Begins by retracting the first section of the bridge using 
+  motor1, then begins retracting the second section of the 
+  bridge using motor2 */
+  
+  //begins turning first motor backwards
+  digitalWrite(motor1bac,HIGH);  //turns first motor backward
+  delay(time1);  //time first motor spins
+  digitalWrite(motor1bac, LOW);  //stops the first motor
+  
+  //begins turning second motor backwards
   digitalWrite(motor2bac, HIGH);
-  delay(time2);
-  digitalWrite(motor2bac, LOW);
+  delay(time2); //time second motors spin
+  digitalWrite(motor2bac, LOW); //stops the second motor
+}
+
+void halt()
+{
+  //Stops all power going to all motors and stops the motion of the bridge deck
+  digitalWrite(motor1for,LOW);
+  digitalWrite(motor1bac,LOW);
+  digitalWrite(motor2for,LOW);
+  digitalWrite(motor2bac,LOW);
 }
 
 
@@ -80,6 +111,10 @@ void loop() {
   
   //---Prepares the Ultrasonic Sensor---//
   
+  /* Initialize the Ultrasonic Sensor with the trigger ping and motion given 
+  by the code below and calculates the distance in centimeters before printing 
+  it into the Arduino monitor */
+  
   // Give a short LOW pulse beforehand to refresh before a HIGH pulse  
   digitalWrite(trigPin, LOW); 
   delayMicroseconds(5);
@@ -89,19 +124,20 @@ void loop() {
   delayMicroseconds(10);
   digitalWrite(trigPin, LOW);
  
-  // Read the signal from the sensor: a HIGH pulse whose
-  // duration is the time (in microseconds) from the sending
-  // of the ping to the reception of its echo off of an object.
+  /* Read the signal from the sensor: a HIGH pulse whose
+  duration is the time (in microseconds) from the sending
+  of the ping to the reception of its echo, off of an object */
+  
   pinMode(echoPin, INPUT);
   duration = pulseIn(echoPin, HIGH);
  
-  // Convert the time into a distance
-  // The speed of sound is: 343m/s = 0.0343 cm/uS = 1/29.1 cm/uS 
-  // or in inches: 13503.9in/s = 0.0135in/uS = 1/74in/uS  
+  /* Convert the time into a distance
+  The speed of sound is: 343m/s = 0.0343 cm/uS = 1/29.1 cm/uS 
+  or in inches: 13503.9in/s = 0.0135in/uS = 1/74in/uS */
   
   cm = (duration/2) / 29.1; // Divide by 29.1 or multiply by 0.0343
 
-  //Prints variable distance in centimeters into monitor
+  //Prints variable distance in centimeters into monitor on the same line as sensor before starting new line
   Serial.print(" Distance: ");
   Serial.print(cm);
   Serial.print("cm");
@@ -116,7 +152,6 @@ void loop() {
   if (sensor == 0)
   {
     extend(4800, 1600);
-    
   }
   
   //Ultra Sonic Sensor
